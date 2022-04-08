737218f09303ee7733e816d8885edea40d54d049
Alistair Smith <hi@alistair.sh>
2022-04-09 00:39:13 +0100

    fix: event types

--- packages/core/src/pipe/managers/rtc.ts
+++ packages/core/src/pipe/managers/rtc.ts
@@ -1,11 +1,20 @@
 import * as mediasoup from 'mediasoup-client';
 import {EventEmitter} from 'events';
+import TypedEmitter from 'typed-emitter';
 
-export class RTCManager extends EventEmitter {
+export type RTCManagerEvents = {
+	CONNECTION_UPDATE: (status: {
+		status: 'connecting' | 'connected' | 'disconnected';
+	}) => void;
+};
+
+export class RTCManager extends (EventEmitter as new () => TypedEmitter<RTCManagerEvents>) {
 	private readonly device: mediasoup.Device;
 	private recvTransport: mediasoup.types.Transport | null;
 
 	constructor() {
+		// Issue with rule — not able to detect our type cast
+		// eslint-disable-next-line constructor-super
 		super();
 		this.device = new mediasoup.Device();
 		this.recvTransport = null;
@@ -21,18 +30,27 @@ export class RTCManager extends EventEmitter {
 		this.recvTransport.on('connectionstatechange', async state => {
 			switch (state) {
 				// We will throw events for each state change
-				case 'connecting':
-					this.emit('ConnectionUpdate', {status: 'connecting'});
+				case 'connecting': {
+					this.emit('CONNECTION_UPDATE', {status: 'connecting'});
 					break;
-				case 'connected':
-					this.emit('ConnectionUpdate', {status: 'connected'});
+				}
+
+				case 'connected': {
+					this.emit('CONNECTION_UPDATE', {status: 'connected'});
 					break;
+				}
+
 				case 'failed':
 				case 'closed':
-				case 'disconnected':
-					this.emit('ConnectionUpdate', {status: 'disconnected'});
-					this.destroy();
+				case 'disconnected': {
+					this.emit('CONNECTION_UPDATE', {status: 'disconnected'});
+					await this.destroy();
 					break;
+				}
+
+				default: {
+					break;
+				}
 			}
 		});
 	}
