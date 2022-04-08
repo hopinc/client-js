a9b69cc668ace6a0f373c22fadb348b167eea00c
slay <me@slayter.dev>
2022-04-09 00:32:06 +0100

    we love events

--- packages/core/src/pipe/managers/rtc.ts
+++ packages/core/src/pipe/managers/rtc.ts
@@ -1,10 +1,12 @@
 import * as mediasoup from 'mediasoup-client';
+import {EventEmitter} from 'events';
 
-export class RTCManager {
+export class RTCManager extends EventEmitter {
 	private readonly device: mediasoup.Device;
 	private recvTransport: mediasoup.types.Transport | null;
 
 	constructor() {
+		super();
 		this.device = new mediasoup.Device();
 		this.recvTransport = null;
 	}
@@ -14,6 +16,24 @@ export class RTCManager {
 
 		this.recvTransport.on('connect', (opts, callback: () => void) => {
 			callback();
+		});
+
+		this.recvTransport.on('connectionstatechange', async state => {
+			switch (state) {
+				// We will throw events for each state change
+				case 'connecting':
+					this.emit('ConnectionUpdate', {status: 'connecting'});
+					break;
+				case 'connected':
+					this.emit('ConnectionUpdate', {status: 'connected'});
+					break;
+				case 'failed':
+				case 'closed':
+				case 'disconnected':
+					this.emit('ConnectionUpdate', {status: 'disconnected'});
+					this.destroy();
+					break;
+			}
 		});
 	}
 
