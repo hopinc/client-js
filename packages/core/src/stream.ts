6d24317af0da23e806f015ac23f1d98d921d5f4d
Alistair Smith <hi@alistair.sh>
2022-04-08 20:40:00 +0100

    feat: stream state

--- packages/core/src/stream.ts
+++ packages/core/src/stream.ts
@@ -3,17 +3,40 @@ export interface NodeConfig {
 	stream: HTMLVideoElement;
 }
 
+export interface HopStreamState {
+	volume: number;
+	muted: boolean;
+}
+
+/**
+ * HopStream manages an instance of a stream to an HTMLVideoElement.
+ */
 export class HopStream {
 	private readonly token;
 	private readonly nodes;
+	private readonly state;
 
 	// TODO: `async` so we can do any resolutions that need to be done here (e.g. finding a node to connect to, etc)
-	public static async from(token: string, nodes: NodeConfig) {
-		return new HopStream(token, nodes);
+	public static async from(
+		token: string,
+		nodes: NodeConfig,
+		state?: Partial<Pick<HopStreamState, 'volume' | 'muted'>>,
+	) {
+		return new HopStream(token, nodes, {
+			volume: 1.0,
+			muted: false,
+			...state,
+		});
 	}
 
-	private constructor(token: string, nodes: NodeConfig) {
+	/**
+	 * Constructor for HopStream
+	 * @param token The join token to connect to a room
+	 * @param nodes The HTML nodes to render and capture to
+	 */
+	private constructor(token: string, nodes: NodeConfig, state: HopStreamState) {
 		this.token = token;
 		this.nodes = nodes;
+		this.state = state;
 	}
 }
