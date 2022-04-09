bf752abd678f8c4267423863f39a7008805bd968
Alistair Smith <hi@alistair.sh>
2022-04-09 19:29:14 +0100

    rtc

--- packages/core/src/pipe/managers/rtc.ts
+++ packages/core/src/pipe/managers/rtc.ts
@@ -8,15 +8,15 @@ export type RTCManagerEvents = {
 };
 
 export class RTCManager extends Emitter<RTCManagerEvents> {
-	private readonly device: mediasoup.Device;
-	private producers: mediasoup.types.Producer[];
-	private consumers: any[];
-	private recvTransport: mediasoup.types.Transport | null;
-
-	private readonly iceServers = [
+	private static readonly iceServers = [
 		{urls: 'stun:stun.l.google.com:19302'},
 		{urls: 'stun:stun1.l.google.com:19302'},
 	];
+
+	private readonly device: mediasoup.Device;
+	private readonly producers: mediasoup.types.Producer[];
+	private readonly consumers: mediasoup.types.Consumer[];
+	private recvTransport: mediasoup.types.Transport | null;
 
 	constructor() {
 		super();
@@ -25,10 +25,12 @@ export class RTCManager extends Emitter<RTCManagerEvents> {
 		this.consumers = [];
 		this.recvTransport = null;
 
-		this.initWebRtc();
+		void this.initWebRtc();
 	}
 
-	async initWebRtc() {}
+	async initWebRtc() {
+		//
+	}
 
 	async createRecvTransport(data: mediasoup.types.TransportOptions) {
 		this.recvTransport = this.device.createRecvTransport(data);
