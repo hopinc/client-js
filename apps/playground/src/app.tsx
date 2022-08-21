dcbbbc054207fa960c6c64c252747a8d0cabbbd0
Alistair Smith <hi@alistair.sh>
2022-08-22 00:41:58 +0100

    fix: improve reconnection logic

--- apps/playground/src/app.tsx
+++ apps/playground/src/app.tsx
@@ -5,7 +5,7 @@ import {useEffect} from 'react';
 const projectId = 'project_Mjg2MjczMDQzMjY4Njg5OTU';
 
 export function Main() {
-	useChannelMessage('messages', 'MESSAGE_SEND', console.log);
+	useChannelMessage('messages', 'MESSAGE_CREATE', console.log);
 
 	useEffect(() => {
 		hop.init({
